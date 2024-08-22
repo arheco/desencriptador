040cefd3592d294b7507d348d060f16ed3eb4739
arheco <rtr.hdez@gmail.com>
2024-08-21 20:17:09 -0600

    init
    First upload! Alura LATAM- ONE G7

---
+++ app.js
@@ -0,0 +1,19 @@
+const vocal_a = 'a';
+const vocal_e = 'e';
+const vocal_i = 'i';
+const vocal_o = 'o';
+const vocal_u = 'u';
+const vocal_1 = 'ai';
+const vocal_2 = 'enter';
+const vocal_3 = 'imes';
+const vocal_4 = 'ober';
+const vocal_5 = 'ufat';
+
+/*La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat"
+*/
+
+
