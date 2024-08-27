2f915172703fe36258c9b737cdec909571ddfc33
arheco <rtr.hdez@gmail.com>
2024-08-27 17:21:48 -0600

    Upgraded to beta 0.5

--- js/app.js
+++ js/app.js
@@ -6,6 +6,11 @@ let campoTextoSalida;
 let botonCopiar;
 let copiar;
 let err; 
+const valtex = /[^A-z\s\d][\\\^]?/g;
+let inputtext;
+let validado;
+var vatx = '';
+
 
 function salidaTexto() {
     document.getElementById("TextoOut").value = textoSaliente;
@@ -14,6 +19,12 @@ function salidaTexto() {
     botonCopiar = document.getElementById("7").style.display= "flex";   
 }
 
+function reset() {
+    document.getElementById("TextoOut").value = textoSaliente;
+    campoMunecoSalida = document.getElementById("MunecoBuscando").style.display = "flex";
+    campoTextoSalida = document.getElementById("CampoSalida").style.display = "none"; 
+    botonCopiar = document.getElementById("7").style.display= "none";   
+}
 
 function busquedaEncriptar() {
     textoIngresado = document.getElementById("EntradaTexto").value;
@@ -25,20 +36,36 @@ function busquedaEncriptar() {
     textoEncriptado = textoEncriptado.replaceAll('u', 'ufat');
 
 textoSaliente = textoEncriptado;
-salidaTexto();
+vatx = validaTexto();
+    if (vatx == false){
+        console.log("El encriptado se detuvo por un error en la validación.");
+        return;
+    }
+        else{
+            console.log("La encriptación se completó correctamente.");
+            salidaTexto();
+    }
 }
 
 function busquedaDesencriptar() {
     textoIngresado = document.getElementById("EntradaTexto").value;
-textoDesencriptado = textoIngresado; 
-textoDesencriptado = textoDesencriptado.replaceAll('ufat', 'u');
-textoDesencriptado = textoDesencriptado.replaceAll('ober', 'o');
-textoDesencriptado = textoDesencriptado.replaceAll('imes', 'i');
-textoDesencriptado = textoDesencriptado.replaceAll('enter', 'e');
-textoDesencriptado = textoDesencriptado.replaceAll('ai', 'a');
+    textoDesencriptado = textoIngresado; 
+    textoDesencriptado = textoDesencriptado.replaceAll('ufat', 'u');
+    textoDesencriptado = textoDesencriptado.replaceAll('ober', 'o');
+    textoDesencriptado = textoDesencriptado.replaceAll('imes', 'i');
+    textoDesencriptado = textoDesencriptado.replaceAll('enter', 'e');
+    textoDesencriptado = textoDesencriptado.replaceAll('ai', 'a');
 
 textoSaliente = textoDesencriptado;
-salidaTexto();
+vatx = validaTexto();
+    if (vatx == false){
+        console.log("El desencriptado se detuvo por un error en la validación.");
+        return;
+    }
+        else{
+            console.log("La desencriptación se completó correctamente.");
+            salidaTexto();
+    }
 }
 
 async function CopiarTexto(){
@@ -49,5 +76,22 @@ async function CopiarTexto(){
     }
         catch (err) {
             console.error ('Error al copiar', err);
+    }
+}
+
+function validaTexto(){
+    inputtext = document.getElementById("EntradaTexto").value;
+    validado = valtex.test(inputtext);    
+if (validado == false) {
+        console.log("Validación OK.");
+        return true;
+    } 
+    else {
+        reset();
+        alert("Tu texto contiene caracteres no admitidos, revisa que solo tengas minúsculas sin acentos y sin signos.");
+        inputtext = '';
+        validado = '';
+        textoIngresado = '';
+        return false;
     }
 }
